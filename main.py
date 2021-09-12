86f10800fe50992d3340aa4bd2ab4a4b8f89af03
Usman-Naeem <usman.naeem2212@gmail.com>
2021-09-12 16:09:08 +0500

    added_category

--- main.py
+++ main.py
@@ -1,42 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://paperswithcode.com')
 
-if response.status_code != 200:
-    print("Not Scraped...", response.status_code)
-    exit()
+def get_data():
+    response = requests.get('https://paperswithcode.com')
+    if response.status_code != 200:
+        print("Not Scraped...", response.status_code)
+        return
 
-soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    mydivs = soup.find_all("div", {"class": "row infinite-item item"})
 
-mydivs = soup.find_all("div", {"class": "row infinite-item item"})
-for div in mydivs:
-    # Get title
-    title = div.find_all('h1')[0].text
+    DATA = []
 
-    # Get Github Url
-    for url in div.find_all('a', href=True):
-        if url['href'].startswith("https://github.com"):
-            github_url = url['href']
-            break
+    for div in mydivs:
 
-    # Get Description
-    for d in div.find_all("p", {"class": "item-strip-abstract"}):
-        description = d.text
-        break
+        # Get title
+        title = div.find_all('h1')[0].text
 
-    # Get Date
-    for d in div.find_all("span", {"class": "author-name-text item-date-pub"}):
-        date = d.text
-        break
-
-    # Get PDF link
-    for d in div.find_all("div", {"class": "entity"}):
-        pdf_response = requests.get("https://paperswithcode.com" + d.find_all('a', href=True)[0]['href'])
-        soup = BeautifulSoup(pdf_response.content, 'html.parser')
-        url_tags = soup.find_all("div", {"class": "paper-abstract"})[0]
-        for url in url_tags.find_all('a', href=True):
-            if url['href'].endswith(".pdf"):
-                pdf_url = url['href']
+        # Get Github Url
+        github_url = ""
+        for url in div.find_all('a', href=True):
+            if url['href'].startswith("https://github.com"):
+                github_url = url['href']
                 break
 
+        # Get Description
+        description = ""
+        for d in div.find_all("p", {"class": "item-strip-abstract"}):
+            description = d.text
+            break
+
+        # Get Date
+        date = ""
+        for d in div.find_all("span", {"class": "author-name-text item-date-pub"}):
+            date = d.text
+            break
+
+        # Get Category
+        category = ""
+        for d in div.find_all("div", {"class": "sota"}):
+            temp = d.text.replace("\n", " ")
+            category = category.join(temp.split("  "))
+
+        # Get PDF link
+        pdf_url = ""
+        for d in div.find_all("div", {"class": "entity"}):
+            pdf_response = requests.get("https://paperswithcode.com" + d.find_all('a', href=True)[0]['href'])
+            soup_2 = BeautifulSoup(pdf_response.content, 'html.parser')
+            url_tags = soup_2.find_all("div", {"class": "paper-abstract"})[0]
+            for url in url_tags.find_all('a', href=True):
+                if url['href'].endswith(".pdf"):
+                    pdf_url = url['href']
+                    break
+        DATA.append({"title": title, "github_url": github_url, "description": description, "date": date,
+                     "category": category, "pdf_url": pdf_url})
+    return DATA
+
+
+data = get_data()
+
+print(len(data))
+
+for d in data:
+    print(d)
